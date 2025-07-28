674a9055fd156672d18bc2b5b4c09b0a08c7e0fa
Kiran Chaitu <119839377+Kiran-Chaitu@users.noreply.github.com>
2025-07-28 14:59:31 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Topological sort/topological-sort.cpp
@@ -0,0 +1,27 @@
+class Solution {
+  public:
+    void dfs(unordered_map< int , vector<int>> &edges  , stack<int> &st , vector<int> &vis , int val){
+        vis[val] =1;
+        for(auto it : edges[val]){
+            if(!vis[it]) dfs(edges , st , vis , it);
+        }
+        st.push(val);
+    }
+    vector<int> topoSort(int V, vector<vector<int>>& edges) {
+        // code here
+        vector<int> ans , vis(V);
+        unordered_map<int,vector<int>> mp;
+        stack<int> st;
+        for(int i = 0 ; i <  edges.size() ; i++){
+            mp[edges[i][0]].push_back(edges[i][1]);
+        }
+        for(int i = 0 ; i < V;i++){
+            if(!vis[i]) dfs(mp , st , vis , i);
+        }
+        while(!st.empty()){
+            ans.push_back(st.top());
+            st.pop();
+        }
+        return ans;
+    }
+};
